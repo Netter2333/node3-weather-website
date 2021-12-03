9336ab5ea1dfe70ad6f1c00eb0a49c9dd3dbbdd5
Bruno <brunokup97@gmail.com>
2021-12-03 20:17:05 -0300

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+	const url =
+		'http://api.openweathermap.org/data/2.5/weather?lat=' +
+		encodeURIComponent(latitude) +
+		'&lon=' +
+		encodeURIComponent(longitude) +
+		'&appid=0deb06148e375ac43ae4e98e52cb264f&units=metric';
+
+	request({ url, json: true }, (error, { body } = {}) => {
+		if (error) {
+			callback('Unable to connect to weather service!', undefined);
+		} else if (body.message) {
+			callback('Error ' + body.message, undefined);
+		} else {
+			const main_data = body.main;
+			const weather_data = body.weather[0];
+			callback(undefined, {
+				weather: weather_data.description,
+				temperature: main_data.temp,
+				feelslike: main_data.feels_like,
+			});
+		}
+	});
+};
+
+module.exports = forecast;
